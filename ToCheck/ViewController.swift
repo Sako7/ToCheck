5a85219d63e30746ada0322871d488cefb114c93
Raffi Megerditchian <raffi.mg@gmail.com>
2018-11-15 10:08:50 -0500

    Initial Commit

---
+++ ToCheck/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToCheck
+//
+//  Created by Raffi Mekerdijian on 11/15/18.
+//  Copyright © 2018 Sarkis Megrditchian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
