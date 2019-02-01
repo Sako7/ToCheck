c0fbef8affa749415299d726137fa61396da6835
Raffi Megerditchian <raffi.mg@gmail.com>
2019-02-01 17:19:33 -0500

    Finished Shortening the code and debugged some things

--- ToCheck/Controller/CheckDoViewController.swift
+++ ToCheck/Controller/CheckDoViewController.swift
@@ -10,17 +10,19 @@ import UIKit
 
 class CheckDoViewController: UITableViewController {
 
-    var itemArray = ["Kill myself", "Get my teacher to be stupid", "be dumb"]
+    var itemArray = [Item]()
     
     let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let newItem = Item()
+        newItem.Title = "lolloloolololo"
+        itemArray.append(newItem)
         // Do any additional setup after loading the view, typically from a nib.
-        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+      if let items = defaults.array(forKey: "TodoListArray") as? [Item] {
             itemArray = items
-            
-            
         }
         
         
@@ -40,7 +42,11 @@ class CheckDoViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Reuse", for: indexPath)
         
-        cell.textLabel?.text = itemArray[indexPath.row]
+        let item = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = item.Title
+        
+        cell.accessoryType = item.Done == true ? .checkmark : .none
         
         return cell
         
@@ -53,13 +59,9 @@ class CheckDoViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
        // print(itemArray[indexPath.row])
         
+        itemArray[indexPath.row].Done = !itemArray[indexPath.row].Done
         
-        
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        tableView.reloadData()
         
         tableView.deselectRow(at: indexPath, animated: true)
         
@@ -76,7 +78,10 @@ class CheckDoViewController: UITableViewController {
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             //What Will happen when You Press AddItem
             
-            self.itemArray.append(textField.text!)
+            let newItem = Item()
+            newItem.Title = textField.text!
+            
+            self.itemArray.append(newItem)
             
             self.defaults.setValue(self.itemArray, forKey: "TodoListArray")
             
---
+++ ToCheck/Data Models/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  ToCheck
+//
+//  Created by Raffi Mekerdijian on 12/14/18.
+//  Copyright © 2018 Sarkis Megrditchian. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Item {
+    var Title : String = ""
+    var Done : Bool = false
+    
+}
